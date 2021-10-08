7435c94e93ffcd42344c73c22c0653abaaf6c9db
Sunil194 <85332465+Sunil194@users.noreply.github.com>
2021-10-08 09:29:02 +0530

    Update main.js
    jquery Display input

--- DispayInput/main.js
+++ DispayInput/main.js
@@ -1,1 +1,16 @@
+var viewModel = function() {
+    var self=this;
+    firstName = ko.observable("sunil"),
+    lastName = ko.observable("kala"),
+    fullName= ko.computed({
+        read: function() {
+        return self.firstName()+" "+ self.lastName();
+    },
+        write: function(val) {
+            var arr = val.split(' ');
+            self.firstName(arr[0]);
+            self.lastName(arr[1]);
 
+        }
+    });
+}
